6106628bc7184dccf118a076c9f3325c93ca6192
ccristianss <cristian.silva9402@gmail.com>
2024-02-23 01:13:40 -0500

    Se actualiza mandaderos

---
+++ app/src/main/java/dev/crsi/manders/ui/UpdateProfileManderActivity.kt
@@ -0,0 +1,12 @@
+package dev.crsi.manders.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.crsi.manders.R
+
+class UpdateProfileManderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update_profile_mander)
+    }
+}
