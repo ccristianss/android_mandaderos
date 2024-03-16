c18a4fa64b26012bb93c5194b72584697229f4a1
quimbaya <john.quimbaya1@misena.edu.co>
2024-03-16 10:38:41 -0500

    cargardo datos1

--- app/src/main/java/dev/crsi/manders/ui/CreateAccountActivity.kt
+++ app/src/main/java/dev/crsi/manders/ui/CreateAccountActivity.kt
@@ -8,5 +8,9 @@ class CreateAccountActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_account)
+
+
     }
+
+
 }
