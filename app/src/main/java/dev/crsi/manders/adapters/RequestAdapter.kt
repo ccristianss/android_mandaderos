d133cda87026734cca7dee1def70fd24f623967f
ccristianss <cristian.silva9402@gmail.com>
2024-03-25 12:13:33 -0500

    Se actualiza Adapters, libreria picasso funcionando correctamente, se agrego android:usesCleartextTraffic="true" al Manifest

--- app/src/main/java/dev/crsi/manders/adapters/RequestAdapter.kt
+++ app/src/main/java/dev/crsi/manders/adapters/RequestAdapter.kt
@@ -1,7 +1,5 @@
 package dev.crsi.manders.adapters
 
-import android.content.Context
-import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,14 +7,11 @@ import androidx.recyclerview.widget.RecyclerView
 import dev.crsi.manders.R
 import dev.crsi.manders.databinding.RequestItemBinding
 import dev.crsi.manders.models.DetailRequestResponse
-import dev.crsi.manders.ui.RequestActivity
 
 class RequestAdapter(private val listRequest: List<DetailRequestResponse>) :
-    RecyclerView.Adapter<DetailRequestViewHolder>() {
-    private lateinit var mContext: Context
+    RecyclerView.Adapter<RequestAdapter.DetailRequestViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DetailRequestViewHolder {
-        mContext = parent.context
         return DetailRequestViewHolder(
             LayoutInflater.from(parent.context).inflate(R.layout.request_item, parent, false)
         )
@@ -27,16 +22,16 @@ class RequestAdapter(private val listRequest: List<DetailRequestResponse>) :
     }
 
     override fun getItemCount(): Int = listRequest.size
-}
 
-class DetailRequestViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-    private val binding = RequestItemBinding.bind(view)
 
-    fun render(data: DetailRequestResponse) {
-        binding.detailRequest.text = data.detail_request
-        binding.statusRequest.text = data.status_request
-        binding.idService.text = data.service_id_service.toString()
+    class DetailRequestViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val binding = RequestItemBinding.bind(view)
+
+        fun render(data: DetailRequestResponse) {
+            binding.detailRequest.text = data.detail_request
+            binding.statusRequest.text = data.status_request
+            binding.idService.text = data.service_id_service.toString()
+        }
     }
-
 }
 
--- app/src/main/java/dev/crsi/manders/adapters/ServicesAdapter.kt
+++ app/src/main/java/dev/crsi/manders/adapters/ServicesAdapter.kt
@@ -1,7 +1,6 @@
 package dev.crsi.manders.adapters
 
-import android.content.Context
-import android.content.Intent
+//import com.bumptech.glide.Glide
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,39 +9,39 @@ import com.squareup.picasso.Picasso
 import dev.crsi.manders.R
 import dev.crsi.manders.databinding.ServicesItemBinding
 import dev.crsi.manders.models.ServiceResponse
-import dev.crsi.manders.ui.RequestActivity
 
 class ServicesAdapter(private val listRequest: List<ServiceResponse>) :
-    RecyclerView.Adapter<ServiceViewHolder>() {
-    private lateinit var mContext: Context
+    RecyclerView.Adapter<ServicesAdapter.ServiceViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ServiceViewHolder {
-        mContext = parent.context
         return ServiceViewHolder(
             LayoutInflater.from(parent.context).inflate(R.layout.services_item, parent, false)
         )
     }
 
     override fun onBindViewHolder(holder: ServiceViewHolder, position: Int) {
-        holder.render(listRequest[position], mContext)
+        holder.render(listRequest[position])
     }
 
     override fun getItemCount(): Int = listRequest.size
-}
 
-class ServiceViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-    private val binding = ServicesItemBinding.bind(view)
 
-    fun render(data: ServiceResponse, context: Context) {
-        binding.nameService.text = data.name_service
-        Picasso.get().load(data.image_service).into(binding.imageService)
-        itemView.setOnClickListener {
-            val intent = Intent(context, RequestActivity::class.java)
-            intent.putExtra("id_service", data.id_service)
-            context.startActivity(intent)
+    class ServiceViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val binding = ServicesItemBinding.bind(view)
+
+        fun render(data: ServiceResponse) {
+            binding.nameService.text = data.name_service
+
+            Picasso.get()
+                .load(data.image_service)
+                .into(binding.imageService)
+
+            /*Glide
+                .with(itemView.context)
+                .load(data.image_service)
+                .into(binding.imageService)*/
         }
+
     }
-
-
 }
 
